aaa411860467d633eb52d5e0bbe0f1d18b190a20
Gabriel Abaya <gabrielabaya08@gmail,com>
2024-06-15 19:43:30 +0800

    sixth commit

--- static/glassesDetails.js
+++ static/glassesDetails.js
@@ -1,52 +1,41 @@
 document.addEventListener('DOMContentLoaded', async(event) => {
     glassJson = await getGlassDetails()
     console.log(glassJson)
-    const descriptionContainer = document.getElementById('details');
-
-
+    const descriptionContainer = document.getElementById('textInfo');
+    const imageSection = document.getElementById("imageSection")
+    let palette = document.getElementById("palette")
     function addDetails(glassClicked) {
-      descriptionContainer.innerHTML = '';
+      descriptionContainer.innerHTML=''
+      imageSection.innerHTML=''
+      palette.innerHTML= ''
+      
       const img = document.createElement('img');
       const glassesName = glassClicked.textContent.trim();
       let glassNumber = extractNumber(glassesName) -1
       img.src = getStaticImageUrl(glassJson[glassNumber]["imageName"]); 
       img.alt = 'Glass Image'; 
 
-      console.log(glassJson)
-      img.style.width = '200px';
-      img.style.height = 'auto';
-
-   
       
-
+    
       const h3 = document.createElement('h3');
       h3.textContent = 'Glass Details';
 
-      const name = document.createElement('p');
-      const sizes = document.createElement('p');
-      const description = document.createElement('p');
-      const shopLink = document.createElement('p');
-      let buttonColor = document.createElement('button');
-
-      buttonColor.classList.add("button")
-      buttonColor.classList.add("button1")
-
-
-
-      shopLink.textContent = glassJson[glassNumber]["Shop Name"];
-      name.textContent = glassJson[glassNumber]["Title"]
-      sizes.textContent = glassJson[glassNumber]["Sizes"]
-      description.textContent =  glassJson[glassNumber]["Description"]
+    
+     
      
 
 
-      descriptionContainer.appendChild(img);
-      descriptionContainer.appendChild(h3);
-      descriptionContainer.appendChild(name);
-      descriptionContainer.appendChild(sizes);
-      descriptionContainer.appendChild(description);
-      descriptionContainer.appendChild(shopLink);
-      createColorButtons(glassJson[glassNumber]["color"],descriptionContainer,glassNumber)
+
+  
+
+      imageSection.appendChild(img)
+      descriptionContainer.appendChild(createParagraph('Eyeglass Name', glassJson[glassNumber]["Title"]));
+      descriptionContainer.appendChild(createParagraph('Sizes', glassJson[glassNumber]["Sizes"]));
+      descriptionContainer.appendChild(createParagraph('Description', glassJson[glassNumber]["Description"]));
+      descriptionContainer.appendChild(createParagraph('Shop', glassJson[glassNumber]["Shop Name"]));
+      descriptionContainer.appendChild(createParagraph('Colors', ''));
+    
+      createColorButtons(glassJson[glassNumber]["color"],palette,glassNumber)
     }
 
     function getStaticImageUrl(filename) {
@@ -74,9 +63,10 @@ document.addEventListener('DOMContentLoaded', async(event) => {
 function createColorButtons(colors, container,number) {
   for (let i = 0; i < colors.length; i++) {
     const buttonColor = document.createElement('button');
-    buttonColor.classList.add("button", "button1");
+    buttonColor.classList.add("button-color");
     buttonColor.style.backgroundColor = colors[i];
     container.appendChild(buttonColor);
+  
     buttonColor.addEventListener('click', () => {
       const clickedButton = event.target;
       const color =  clickedButton.style.backgroundColor;
@@ -85,5 +75,24 @@ function createColorButtons(colors, container,number) {
     });
   }
 }
+function remove(){
+  const descriptionContainer = document.getElementById('textInfo');
+  const imageSection = document.getElementById("imageSection")
+  let palette = document.getElementById("palette")
+  descriptionContainer.innerHTML=''
+  imageSection.innerHTML=''
+  palette.innerHTML=''
+}
+
+const createParagraph = (labelText, valueText) => {
+  const p = document.createElement('p');
+  const label = document.createElement('span');
+  label.className = 'label';
+  label.textContent = labelText;
+  p.appendChild(label);
+  p.append(valueText);
+  return p;
+};
+
 
 
